45bc363dc2db10167487898464bd15f44ace1dcf
HFrench20 <h.french.1@research.gla.ac.uk>
2021-03-30 13:08:44 +0100

    List of adm2 keys to add using the individual "additions" method.

---
+++ manuscript/data/cleaning/spare_adm2_keys_to_add.R
@@ -0,0 +1,55 @@
+ARG.10_1.additions <- c("ARG.10.1_1") # dr. manuel belgrano = capital
+ARG.17_1.additions <- c("ARG.17.8_1") # general jose de san martin
+BOL.2_1.additions <- c("BOL.2.2_1", "BOL.2.1_1") # Bolivar = arque, tiraque = arani
+BOL.4_1.additions <- c("BOL.4.17_1") # Murillo = Pedro Domingo Murillo
+BOL.8_1.additions <- c("BOL.8.6_1", "BOL.8.10_1") # Warnes = Ignacio Warnes; Obispo Santisteban = Obispo Santistevan
+BRA.5_1.additions <- c("BRA.5.81_1") # cansanaao = cancacao 
+BRA.10_1.additions <- c("BRA.10.130_1", "BRA.10.86_1") # ; humberto de campos = humberto campos  
+BRA.11_1.additions <- c("BRA.11.68_1") # ladario = sapezal 
+COL.5_1.additions <- c("COL.5.8_1") # magangue = cicuco
+COL.19_1.additions <- c("COL.19.19_1", " COL.19.2_1") #  santa marta = santa marta (dist. esp.), ariguani(el dificil) = ariguani 
+COL.29_1.additions <- c("COL.29.40_1") # mariquita = san sebastian de mariquita
+#CUB.2_1.additions <-  c("CUB.2.9_1") # iro de enero = primero de enero  
+#CUB.3_1.additions <-  c("CUB.3.3_1", "CUB.3.2_1") # cienfuegos = ciefuegos; aguada = aguada de pasajeros
+#CUB.4_1.additions <- c("")
+#CUB.11_1.additions <- 
+#CUB.13_1.additions <- 
+#CUB.16_1.additions <- 
+DOM.6_1.additions <- c("DOM.6.6_1") # san fco. de macoris = san francisco de macoris
+DOM.7_1.additions <- c("DOM.7.2_1") # el seybo = santa cruz del seybo
+DOM.9_1.additions <- c("DOM.9.2_1") # hato mayor = hato mayor del rey
+DOM.11_1.additions <- c("DOM.11.2_1", "Sin Informacion") # higuey = salvaleon de higuey; altagracia = name of province
+DOM.13_1.additions <- c("DOM.13.2_1") # romana = la romana
+DOM.23_1.additions <- c("DOM.23.3_1") # samana = santa barbara de samana
+DOM.26_1.additions <- c("DOM.26.5_1", "DOM.26.5_1") # s.j.de la maguana = san juan de la maguana;  san juan = san juan de la maguana
+DOM.27_1.additions <- c("DOM.27.4_1", "DOM.27.3_1") #  ing. Quisquella = quisquella; san jose de los llanos = los llanos 
+DOM.31_1.additions <- c("Sin Informacion", "Sin Informacion", "Sin Informacion", "DOM.31.6_1") # santo domingo = state name!; villa mella and distrito nacional = STATE OF distrito nacional; la victoria = santo domingo norte
+SLV.2_1.additions <- c("SLV.2.2_1") # villa dolores = dolores  
+SLV.4_1.additions <- c("SLV.4.9_1", "SLV.4.7_1") # s.jose guayabal = san jose guayabal; s.b.perulapia = san bartolome perulapia 
+SLV.5_1.additions <- c("SLV.5.12_1", "SLV.5.3_1") # san juan opico = opico; arce = ciudad arce 
+SLV.7_1.additions <- c("SLV.7.16_1") # sta rosa de lima = santa rosa de lima
+SLV.9_1.additions <- c("SLV.9.18_1", "SLV.9.16_1") # san rafael oriente = san rafael; san luis de la reyna = san luis de la reina 
+SLV.10_1.additions <- c("SLV.10.6_1", "SLV.10.5_1") #  el paisal = el paisnal; ciudad delgado = delgado   
+SLV.12_1.additions <- c("SLV.12.1_1") # cand.de la frontera = candelaria de la frontera
+SLV.13_1.additions <- c("SLV.13.10_1") # s.ant.del monte = san antonio del monte
+SLV.14_1.additions <- c("SLV.14.11_1", "SLV.14.6_1", "SLV.14.16_1", "SLV.14.19_1") # m.umana = mercedes umana; ereguaiquin = ereguayquin; san buena ventura = san buenaventura; sta elena = santa elena
+GTM.1_1.additions <- c("GTM.1.4_1") # fray b. de las cajas =  fray bartolome de las casas
+GTM.3_1.additions <- c("GTM.3.11_1") # s.m.jilotepeque = san martin jilotepeque 
+GTM.5_1.additions <- c("GTM.5.4_1") # s.a.acasaguastlan = san agustin acasaguastlan
+GTM.6_1.additions <-c("GTM.6.12_1") # siquinalan = siquinala
+GTM.7_1.additions <- c("Sin Informacion", "GTM.7.12_1", "GTM.7.10_1") # guatemala = Sin Informacion; san pdo. Sacatepequez = san pedro sacatepequez; s.j.sacatepequez = san juan sacatepequez    
+GTM.8_1.additions <- c("GTM.8.21_1", "GTM.8.14_1", "GTM.8.10_1", "GTM.8.10_1", "GTM.8.10_1", "GTM.8.10_1") # san rafael = san rafael petzal; ildefonso ixtahuacan = san ildefonso ixtahuacan; all other are Malacatancito based on coordinates.
+GTM.13_1.additions <- c("GTM.13.14_1", "GTM.13.16_1", "GTM.13.16_1", "GTM.13.7_1", "GTM.13.16_1", "GTM.13.15_1", "GTM.13.15_1") # san juan oztuncalco = ostuncalco; zona 1, zona 5, zona 3 = quetzaltenango; concepcion = concepcion chiquirichapa; palestina, p.de los altos = palestina de los altos 
+GTM.14_1.additions <- c("GTM.14.15_1", "GTM.14.19_1", "GTM.14.17_1") # santo antonio ilote = san antonio ilotenango; santa cruz = santa cruz del quiche; cotzal = san juan cotzal 
+GTM.17_1.additions <- c("GTM.17.16_1", " GTM.17.25_1", "GTM.17.1_1", "GTM.17.6_1", "GTM.17.27_1") #san antonio = san antonio sacatepequez; sibinal = san sibinal; tecunuman = ayutla; san jose el rode = el rodeo;  tasana = tacana
+GTM.19_1.additions <- c("GTM.19.13_1") #  ixtahuacan = santa catarina ixtahuacan
+GTM.20_1.additions <- c("GTM.20.10_1", "GTM.20.19_1") # s.f.zapotitlan = san francisco zapotitlan; santo tomas = santo tomas la union
+GTM.21_1.additions <- c("GTM.21.4_1", "GTM.21.5_1") # san cristobal = san cristobal totonicapan; san francisco = san francisco el alto
+MEX.5_1.additions <- c("MEX.5.104_1") # venustiano carr. = venustiano carranza 
+MEX.15_1.additions <- c("Sin Informacion") # mexico = state name!
+MEX.22_1.additions <- c("MEX.22.7_1") # e.montes = ezequiel montes
+MEX.31_1.additions <- c("MEX.31.79_1") # tekak = tekax 
+NIC.13_1.additions <- c("NIC.13.2_1") # silais = managua
+PRY.7_1.additions <- c("PRY.7.4_1") # buena vista = emboscada (caazapa)
+PER.16_1.additions <- c("Sin Informacion") # Lima = city, so wrong state.
+VEN.25_1.additions <- c("VEN.25.8_1", "VEN.25.7_1") # jesus enrique lozada = jesus enrique lossada; paez = guajira
